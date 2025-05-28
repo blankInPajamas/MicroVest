aaaf67cbd2d70b48aaaffa9dddbd861ed1f7214a
Ishmam <ahmedishmam59@gmail.com>
2025-05-29 01:48:43 +0600

    Added the Django files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'
 import LandingPage from './pages/LandingPage/LandingPage'
+import SignUpPage from './pages/Sign up/sign-up-page';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route path='/signup' element={<SignUpPage />} />
       </Routes>
     </>
   )
